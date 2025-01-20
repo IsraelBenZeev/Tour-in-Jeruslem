de8d415efbf41b8a03a5dce2089b76c5f89fe0b6
ibz1330 <ibz0502291330@gmail.com>
2025-01-21 01:12:56 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+document.addEventListener("scroll", () => {
+  console.log("scroling 1");
+  const cards1 = document.querySelector(".cards1"); // בוחר את האלמנט
+  const rect = cards1.getBoundingClientRect(); // בודק את המיקום שלו יחסית למסך
+
+  if (rect.top < window.innerHeight && rect.bottom > 0) {
+    // אם האלמנט נכנס ל-viewport
+    cards1.classList.add("visible");
+  } else {
+    // אם האלמנט יוצא מה-viewport (אופציונלי)
+    cards1.classList.remove("visible");
+  }
+});
+
+document.addEventListener("scroll", () => {
+  console.log("scroling 2");
+  const cards2 = document.querySelector(".cards2"); // בוחר את האלמנט
+  const rect = cards2.getBoundingClientRect(); // בודק את המיקום שלו יחסית למסך
+
+  if (rect.top < window.innerHeight && rect.bottom > 0) {
+    // אם האלמנט נכנס ל-viewport
+    cards2.classList.add("visible");
+  } else {
+    // אם האלמנט יוצא מה-viewport (אופציונלי)
+    cards2.classList.remove("visible");
+  }
+});
+
+document.addEventListener("scroll", () => {
+  console.log("scroling 3");
+  const jerusalemNumber = document.querySelector(".jerusalem-number"); // בוחר את האלמנט
+  const rect = jerusalemNumber.getBoundingClientRect(); // בודק את המיקום שלו יחסית למסך
+
+  if (rect.top < window.innerHeight && rect.bottom > 0) {
+    // אם האלמנט נכנס ל-viewport
+    jerusalemNumber.classList.add("visible");
+  } else {
+    // אם האלמנט יוצא מה-viewport (אופציונלי)
+    jerusalemNumber.classList.remove("visible");
+  }
+});
+
+
+const sideMenu = document.getElementById('mobile-menu');
+const hamburger = document.getElementById('hamburger');
+const buttonClose = document.getElementById('close');
+
+hamburger.addEventListener('click', function() {
+  // מחזירים את התפריט למצב רגיל לפני הפתיחה
+  sideMenu.style.transition = 'none'; // מסירים את האנימציה הקודמת
+  sideMenu.style.transform = 'translateX(0)';
+  sideMenu.style.opacity = '1';
+
+  sideMenu.style.display = 'flex';
+  hamburger.style.display = 'none';
+  
+  // מוסיפים אנימציה מחדש לאחר רגע, כך שהיא תתבצע בזמן הבא שלפתיחה מחדש
+  setTimeout(function() {
+    sideMenu.style.transition = 'transform 1s, opacity 1s'; // מוסיפים את האנימציה מחדש
+  }, 10);
+});
+
+buttonClose.addEventListener('click', function() {
+  // מפעילים את האנימציה של החלקה החוצה
+  sideMenu.style.transition = 'transform 1s, opacity 1s';
+  sideMenu.style.transform = 'translateX(100%)';
+  sideMenu.style.opacity = '0';
+
+  // לאחר סיום האנימציה, נסיר את התצוגה
+  setTimeout(function() {
+    sideMenu.style.display = 'none'; // הסתרת התפריט לאחר האנימציה
+    hamburger.style.display = 'flex'; // הצגת ההמבורגר
+  }, 1000); // מחכים עד שהאנימציה תסתיים
+});
+
+
+
+
